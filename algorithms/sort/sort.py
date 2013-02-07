8c2682d38c92c933ce9828b78b7c37109642ea06
Rahul Gupta <rahulgupta@Rahuls-MacBook-Air.local>
2013-02-07 14:33:51 -0800

    wrote merge sort.
    Merge sort might need to be cleaned up. The test seems to pass

--- algorithms/sort/sort.py
+++ algorithms/sort/sort.py
@@ -8,3 +8,31 @@ def insertion_sort(arr):
         arr[j + 1] = key
     print arr
 
+def merge_sort(arr, beg, end):
+    if beg + 1 >= end:
+        return
+    mid = (beg + end)/2
+    merge_sort(arr, beg, mid)
+    merge_sort(arr, mid, end)
+    left = []
+    right = []
+    for i in range(beg, mid):
+        left.append(arr[i])
+    for j in range(mid, end):
+        right.append(arr[j])
+    i = 0
+    j = 0
+    for k in range(beg, end):
+        if i >= len(left):
+            arr[k] = right[j]
+            j = j + 1
+        elif j >= len(right):
+            arr[k] = left[i]
+            i = i + 1
+        elif left[i] < right[j]:
+            arr[k] = left[i]
+            i = i + 1
+        else:
+            arr[k] = right[j]
+            j = j + 1
+    print arr
--- algorithms/sort/tests.py
+++ algorithms/sort/tests.py
@@ -1,3 +1,7 @@
 from sort import *
 
 insertion_sort([1, 4, 5, 3])
+
+
+arr = [3, 4, 1, 10, 19, 35, 1, 5, 8]
+merge_sort(arr, 0, len(arr))
